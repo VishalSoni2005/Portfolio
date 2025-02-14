a5c1208b0d628b419d3ebeb4b494a93e3cb255eb
VishalSoni007 <vsoni0882@gmail.com>
2025-02-14 23:13:04 +0530

    ResponsiveNess

--- src/Layout/05_Timeline.jsx
+++ src/Layout/05_Timeline.jsx
@@ -1,3 +1,5 @@
+
+
 export default function CardFive() {
   return (
     <div className="h-screen bg-[#f2f2f2] text-black flex items-center justify-center overflow-hidden rounded-xl">
@@ -5,13 +7,14 @@ export default function CardFive() {
         <div className="container max-w-5xl px-6 py-16 mx-auto">
           <div className="grid gap-6 mx-4 sm:grid-cols-12">
             <div className="col-span-12 sm:col-span-3">
-              <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-green-500">
+              <div className="hidden sm:block text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-green-500">
                 <h3 className="text-4xl font-bold font-serif">Education</h3>
               </div>
             </div>
 
             <div className="relative col-span-12 px-4 space-y-8 sm:col-span-9">
               <div className="col-span-12 space-y-10 relative px-4 sm:col-span-8 sm:space-y-10 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-300">
+
                 <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-5 sm:before:h-5 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-green-400 hover:shadow-2xl transition-all duration-500 rounded-xl p-6 bg-white">
                   <h3 className="text-2xl font-semibold tracking-wide">
                     Bachelor of Technology in Information Technology
@@ -45,6 +48,7 @@ export default function CardFive() {
                   </p>
                   <p className="mt-3 text-lg font-medium">Percentage: 88.9%</p>
                 </div>
+
               </div>
             </div>
           </div>
--- src/Layout/06_SocialLinks.jsx
+++ src/Layout/06_SocialLinks.jsx
@@ -2,16 +2,24 @@ import DecryptedText from '../components/custom_components/Text_Component/Decryp
 
 export default function CardFive() {
   return (
-    <div className="h-[400px] bg-black font-serif text-black flex flex-col items-center justify-center overflow-hidden rounded-xl ">
+    <div className="h-[400px] bg-black font-serif text-gray-400 flex flex-col items-center justify-center overflow-hidden rounded-xl ">
       {/* LinkedIn */}
       <div style={{ marginTop: '2rem' }}>
         <div className="group relative cursor-pointe text-8xl uppercase">
-          <DecryptedText
-            text="LinkedIn"
-            animateOn="view"
-            revealDirection="center"
-            className="text-gray-400 group-hover:text-blue-600 transition duration-300"
-          />
+          <button
+            rel="noopener noreferrer"
+            className='group relative cursor-pointer text-8xl uppercase'
+            onClick={() =>
+              window.open('https://www.linkedin.com/in/vishal-soni-07-/')
+            }
+          >
+            <DecryptedText
+              text="LinkedIn"
+              animateOn="view"
+              revealDirection="center"
+              className="text-gray-400 group-hover:text-blue-600 transition duration-300"
+            />
+          </button>
           {/* Underline animation */}
           <div className="absolute bottom-[-2px] left-0 w-0 h-[2px] bg-blue-500 group-hover:w-full transition-all duration-500"></div>
         </div>
@@ -22,12 +30,20 @@ export default function CardFive() {
         className="group relative cursor-pointer text-8xl uppercase"
         style={{ marginTop: '2rem' }}
       >
-        <DecryptedText
-          text="Instagram"
-          animateOn="view"
-          revealDirection="center"
-          className="text-gray-400 group-hover:text-pink-700 transition duration-300"
-        />
+        <button
+          rel="noopener noreferrer"
+          className='group relative cursor-pointer text-8xl uppercase'
+          onClick={() =>
+            window.open('https://www.instagram.com/')
+          }
+        >
+          <DecryptedText
+            text="Instagram"
+            animateOn="view"
+            revealDirection="center"
+            className="text-gray-400 group-hover:text-pink-700 transition duration-300"
+          />
+        </button>
         {/* Underline animation */}
         <div className="absolute bottom-[-2px] left-0 w-0 h-[2px] bg-pink-500 group-hover:w-full transition-all duration-500"></div>
       </div>
